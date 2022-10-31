bd7b85dc5459ee406cddda3759c32a0d861c559e
@hexlet/cli <support@hexlet.io>
2022-10-31 22:22:29 +0300

    submit rails-basics-ru/rack

--- rails-basics-ru/rack/lib/app/execution_timer.rb
+++ rails-basics-ru/rack/lib/app/execution_timer.rb
@@ -8,7 +8,7 @@ class ExecutionTimer
   def call(env)
     prev_time = Time.now
     status, headers, body = @app.call(env)
-    time_in_miliseconds = ((Time.now - prev_time) * 1000).round
+    time_in_miliseconds = ((Time.now - prev_time) * 1000).ceil
 
     [status, headers, [body, "Execution time: #{time_in_miliseconds}ms"].join('\n')]
   end
