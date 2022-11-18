308c8082aeffa278b3978466b0fcdf72ba573022
@hexlet/cli <support@hexlet.io>
2022-11-18 08:56:24 +0300

    submit rails-basics-ru/templates

--- rails-basics-ru/templates/app/models/task.rb
+++ rails-basics-ru/templates/app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :status, presence: true
+  validates :creator, presence: true
+  validates :completed, inclusion: [true, false]
 end
