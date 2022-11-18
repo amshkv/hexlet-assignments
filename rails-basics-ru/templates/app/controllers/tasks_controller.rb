a794b669969fc48c132be9a9d5ed8366952a371f
@hexlet/cli <support@hexlet.io>
2022-11-18 08:55:05 +0300

    submit rails-basics-ru/templates

---
+++ rails-basics-ru/templates/app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:notice] = 'Task was successfully created.'
+      redirect_to task_path(@task)
+    else
+      flash[:alert] = 'Task was not created.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:notice] = 'Task was successfully updated.'
+      redirect_to task_path(@task)
+    else
+      flash[:alert] = 'Task was not updated.'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+
+    @task.destroy
+    flash[:notice] = 'Task was successfully deleted.'
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :status, :creator, :performer, :completed)
+  end
+end
