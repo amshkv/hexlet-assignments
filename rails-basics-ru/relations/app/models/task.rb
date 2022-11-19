1f9e6a743b7bba1802dcf0cdd2a8c405311756ed
@hexlet/cli <support@hexlet.io>
2022-11-19 15:47:37 +0300

    submit rails-basics-ru/relations

--- rails-basics-ru/relations/app/models/task.rb
+++ rails-basics-ru/relations/app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+
   belongs_to :user
   belongs_to :status
 end
--- rails-basics-ru/relations/test/controllers/tasks_controller_test.rb
+++ rails-basics-ru/relations/test/controllers/tasks_controller_test.rb
@@ -1,7 +1,59 @@
-require "test_helper"
+require 'test_helper'
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @task = tasks(:one)
+
+    @attrs = {
+      name: Faker::Movies::PrincessBride.character,
+      user_id: users(:full).id,
+      status_id: statuses(:done).id
+    }
+  end
+
+  test 'should get index' do
+    get tasks_url
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_task_url
+    assert_response :success
+  end
+
+  test 'should create task' do
+    post tasks_url, params: { task: @attrs }
+
+    task = Task.find_by @attrs
+
+    assert { task }
+    assert_redirected_to task_url(task)
+  end
+
+  test 'should show task' do
+    get task_url(@task)
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get edit_task_url(@task)
+    assert_response :success
+  end
+
+  test 'should update task' do
+    patch task_url(@task), params: { task: @attrs }
+
+    @task.reload
+
+    assert { @task.name == @attrs[:name] }
+    assert_redirected_to task_url(@task)
+  end
+
+  test 'should destroy task' do
+    delete task_url(@task)
+
+    assert { !Task.exists? @task.id }
+
+    assert_redirected_to tasks_url
+  end
 end
