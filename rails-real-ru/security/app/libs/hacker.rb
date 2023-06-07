06de7a877985cc2745928f0db337f8685c21baef
@hexlet/cli <support@hexlet.io>
2023-06-07 22:10:35 +0300

    submit rails-real-ru/security

--- rails-real-ru/security/app/libs/hacker.rb
+++ rails-real-ru/security/app/libs/hacker.rb
@@ -7,64 +7,43 @@ class Hacker
   class << self
     def hack(email, password)
       # BEGIN
-      uri = URI.join(hostname, '/users/sign_up')
-      response = get_response(uri)
-      html = Nokogiri::HTML(response.body)
+      hostname = 'https://rails-collective-blog-ru.hexlet.app/'
+      reg_page_path = 'users/sign_up'
+      reg_action_path = 'users'
 
-      authenticity_token = get_authenticity_token(html)
-      cookie = get_cookie(response)
-
-      params = set_params(email, password, authenticity_token)
-
-      form_uri = URI.join(hostname, '/users')
-      request(form_uri, params, cookie)
-    end
-
-    def hostname
-      'https://rails-collective-blog-ru.hexlet.app/'
-    end
-
-    def get_response(url)
-      uri = URI(url)
+      uri = URI(hostname)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = uri.scheme == 'https'
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      request = Net::HTTP::Get.new uri
+      request = Net::HTTP::Get.new URI.join(hostname, reg_page_path)
 
-      http.request request
-    end
+      response = http.request request
 
-    def get_cookie(response)
-      response.response['set-cookie'].split('; ')[0]
-    end
+      cookie = response.response['set-cookie'].split('; ')[0]
 
-    def get_authenticity_token(html)
-      authenticity_token_tag = html.at('input[name="authenticity_token"]')
-      authenticity_token_tag['value']
-    end
-
-    def set_params(email, password, token)
-      {
-        authenticity_token: token,
-        user: {
-            email: email,
-            password: password,
-            password_confirmation: password
-        }
+      params = {
+        'user[email]': email,
+        'user[password]': password,
+        'user[password_confirmation]': password,
+        authenticity_token: auth_token(response.body)
       }
-    end
 
-    def request(uri, params, cookie)
-      request = Net::HTTP::Post.new uri
+      request = Net::HTTP::Post.new URI.join(hostname, reg_action_path)
       request.body = URI.encode_www_form(params)
       request['Cookie'] = cookie
 
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = uri.scheme == 'https'
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      response = http.request request
 
-      http.request request
+      response.code == '302'
+    end
+
+    private
+
+    def auth_token(body)
+      html = Nokogiri::HTML(body)
+
+      html.at('input[@name="authenticity_token"]')['value']
     end
     # END
   end
